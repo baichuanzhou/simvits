0206e6859527453da7f9d08c4f9ac4a87c68db82
baichuanzhou <baichuanzhou44@gmail.com>
2023-06-28 08:46:43 +0800

    cifar10 run updates

--- src/test.py
+++ src/test.py
@@ -1,8 +1,9 @@
-from vision_transformers import VisionTransformer, Trainer, TrainingArguments
+from vision_transformers import VisionTransformer, Trainer, TrainingArguments, compute_metrics
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-
+from typing import Any, Tuple
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 import torchvision.transforms as T
@@ -24,13 +25,6 @@ if __name__ == '__main__':
     NUM_TRAIN = 45000
     NUM = 50000
 
-    optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay=0.1)
-    base_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200,
-                                                                eta_min=1e-5)
-    lr_scheduler = warmup_scheduler.GradualWarmupScheduler(optimizer, multiplier=1,
-                                                           total_epoch=5, after_scheduler=base_scheduler)
-
-    torch.set_float32_matmul_precision('high')
     train_transform = T.Compose([
         T.RandomCrop(32, padding=4),
         T.RandomHorizontalFlip(),
@@ -67,23 +61,27 @@ if __name__ == '__main__':
         batch_size=512,
         shuffle=False
     )
+
     training_args = TrainingArguments(
-        output_dir="../temp_output",
+        output_dir="./temp_output",
         do_train=True,
+        do_eval=True,
+        do_predict=True,
         optim="adamw",
         logging_steps=100,
         learning_rate=1e-3,
         weight_decay=0.1,
         lr_scheduler_type="cosine",
-        warmup_steps=500,
-        epoch=200
+        warmup_steps=3000,
+        epoch=100
     )
     trainer = Trainer(
         model=model,
         training_args=training_args,
         train_loader=train_loader,
+        eval_loader=val_loader,
+        test_loader=test_loader,
         criterion=F.cross_entropy,
-        # optimizer=optimizer,
-        # lr_scheduler=lr_scheduler
+        compute_metric=compute_metrics
     )
     trainer.train()
